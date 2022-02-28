a50c311492b702e9f71437fbd4aa8b9fd10b8747
hanq <hanq@deha-soft.com>
2022-02-28 16:30:09 +0700

    learn router and axios

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-    <!-- Display event data -->
-    
-  </div>
+  <router-link class="event-link" :to="{ name: 'EventDetail', params: { id: event.id } }">  
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+      <!-- Display event data -->
+      
+    </div>
+  </router-link>
 </template>
 
 <script>
