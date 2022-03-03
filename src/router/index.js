c4f7a43a34b856788647a4858622df4d785e30a2
hanq <hanq@deha-soft.com>
2022-03-03 13:55:03 +0700

    paginate data

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import EventDetail from "../views/EventDetail.vue";
-import About from "../views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import About from '../views/About.vue'
+import EventDetail from '../views/EventDetail.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList,
+    props: route => ({
+      limit: parseInt(route.query.limit) || 2,
+      page: parseInt(route.query.page) || 1
+    })
   },
   {
-    path: "/event/:id",
-    name: "EventDetail",
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetail',
     props: true,
     component: EventDetail,
   },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from 'axios'
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/elkun310/real-world-vue',
-    withCredentials: false,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-  
-  export default {
-      getEvents() {
-          return apiClient.get('/events');
-      },
-      getEvent(id) {
-        return apiClient.get('/events/' + id);
-    }
-  }
+  baseURL: 'https://my-json-server.typicode.com/elkun310/real-world-vue',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+  },
+  getEvent(id) {
+    return apiClient.get(`/events/${id}`)
+  },
+}
