38c4d933b8c7db07d3220f7d84684bc8fce320af
hanq <hanq@deha-soft.com>
2022-03-03 13:45:16 +0700

    add more data

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,39 +1,92 @@
 <template>
+  <h1>Events For Good</h1>
   <div class="events">
-  <h1>Events for Good</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" msg="Welcome to Your Vue.js App11111" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link :to="{name: 'EventList', query: {page: page - 1, limit: limit}}"
+      rel="prev"
+      v-if="page != 1"
+      id="page-prev"
+      >&#60; Prev</router-link>
+
+      <router-link :to="{name: 'EventList', query: {page: page + 1, limit: limit}}"
+      rel="next"
+      v-if="hasNextPage"
+      id="page-next"
+      >Next &#62;</router-link>
+    </div>
+
+    <select name="" id="per-page" @change="changeLimit($event)">
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+      <option :value="4">4</option>
+    </select>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import EventCard from "@/components/EventCard.vue";
-import EventService from '@/services/EventService.js';
+import EventCard from '@/components/EventCard.vue'
+import EventService from '../services/EventService'
+import {watchEffect} from 'vue';
+
 export default {
-  name: "Home",
+  name: 'EventList',
+  props: ['page', 'limit'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents : 0,
+    }
+  },
+  methods: {
+    changeLimit(event) {
+      this.$router.push('?limit='+event.target.value+'&page=' + this.page)
     }
   },
   created() {
-    EventService.getEvents()
-    .then(response => {
-      this.events = response.data;
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(this.limit, this.page)
+            .then((response) => {
+              this.events = response.data;
+              this.totalEvents = response.headers['x-total-count'];
+            })
+            .catch((error) => {
+              console.log(error)
+            })
     })
-    .catch(error => {
-      console.log(error)
-    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages;
+    }
   }
-};
+}
 </script>
 <style scoped>
 .events {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e52;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
